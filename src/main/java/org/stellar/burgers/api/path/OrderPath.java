55d0f6f8014151715e4d6580afdc9a8a70bbc6b6
Elena Agarkova <lenaas87@bk.ru>
2023-09-15 14:33:28 +0300

    Добавлены маршруты

---
+++ src/main/java/org/stellar/burgers/api/path/OrderPath.java
@@ -0,0 +1,6 @@
+package org.stellar.burgers.api.path;
+
+public class OrderPath {
+    public static final String BASIC_ROUTE = "/orders";
+    public static final String ALL_ROUTE = BASIC_ROUTE + "/all";
+}
